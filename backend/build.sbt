ae5694b23958a7fe38890f9f4825f3551d707322
Adriano Melo <github@adrianomelo.com>
2020-08-22 18:48:55 +0200

    First commit

---
+++ backend/build.sbt
@@ -0,0 +1,12 @@
+scalaVersion := "2.12.12"
+
+val akkaVersion = "2.6.8"
+val akkaHttpVersion = "10.2.0"
+val akkaHttpCorsVersion = "1.1.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "ch.megard" %% "akka-http-cors" % akkaHttpCorsVersion
+)
---
+++ backend/src/main/scala/chat/Server.scala
@@ -0,0 +1,94 @@
+package chat
+
+import akka.NotUsed
+import akka.actor._
+import akka.http.scaladsl._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.FromRequestUnmarshaller
+import akka.stream._
+import akka.stream.scaladsl._
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import scala.io.StdIn
+
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+
+object Server {
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+
+    val chatRoom = system.actorOf(Props(new ChatRoom), "chat")
+
+    def newUser(): Flow[Message, Message, NotUsed] = {
+      // new connection - new user actor
+      val userActor = system.actorOf(Props(new User(chatRoom)))
+
+      val incomingMessages: Sink[Message, NotUsed] =
+        Flow[Message].map {
+          // transform websocket message to domain message
+          case TextMessage.Strict(text) => User.IncomingMessage(text)
+          case _ => User.IncomingMessage("??")
+        }.to(Sink.actorRef[User.IncomingMessage](userActor, PoisonPill))
+
+      val outgoingMessages: Source[Message, NotUsed] =
+        Source.actorRef[User.OutgoingMessage](10, OverflowStrategy.fail)
+        .mapMaterializedValue { outActor =>
+          // give the user actor a way to send messages out
+          userActor ! User.Connected(outActor)
+          NotUsed
+        }.map(
+          // transform domain message to web socket message
+          (outMsg: User.OutgoingMessage) => TextMessage(outMsg.text))
+
+      // then combine both to a flow
+      Flow.fromSinkAndSource(incomingMessages, outgoingMessages)
+    }
+
+    val route =
+      concat (
+        path("public.gif") {
+          get {
+            extractHost { host =>
+              extractRequest { context =>
+                chatRoom ! ChatRoom.ChatMessage(context.toString())
+                chatRoom ! ChatRoom.ChatMessage(host.toString())
+                
+                //chatRoom ! ChatRoom.ChatMessage(context.getHeader("Referer").get().value())
+
+                complete("OK")
+              }
+            }
+          }
+        },
+
+        path("browser") {
+        concat (
+          get {
+            handleWebSocketMessages(newUser())
+          },
+
+          post {
+            cors() {
+              entity(as[String]) { data =>
+                chatRoom ! ChatRoom.ChatMessage(data)
+                complete("OK")
+              }
+            }
+          }
+        )
+      }
+      )
+
+    val binding = Await.result(Http().bindAndHandle(route, "127.0.0.1", 5000), 3.seconds)
+
+
+    // the rest of the sample code will go here
+    println("Started server at 127.0.0.1:5000, press enter to kill server")
+    StdIn.readLine()
+    system.terminate()
+  }
+}
